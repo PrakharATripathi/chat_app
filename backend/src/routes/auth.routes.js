06a0f03f88e5fe6e80f7ef94f37a3a165a56177f
Prakhar-Ordex <prakhar.tripathi.ordex@gmail.com>
2025-02-17 16:27:04 +0530

    update

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,8 +3,8 @@ import express from "express";
 const router = express.Router();
 
 router.post('/signup');
-router.post('/signin')
-router.post('/logout')
+router.post('/signin');
+router.post('/logout');
 
 export default router;
 
