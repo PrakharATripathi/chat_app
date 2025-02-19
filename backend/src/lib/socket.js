3b5c127f00be89a5ce1b64505d17075438308360
Prakhar-Ordex <prakhar.tripathi.ordex@gmail.com>
2025-02-19 15:40:25 +0530

    update

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,8 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://chattya.netlify.app"],
-    credentials: true,
+    origin: ["http://localhost:5173"],
   },
 });
 
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:  "https://chat-app-t0e4.onrender.com/api",
+  baseURL:  "http://localhost:3000/api",
   withCredentials: true,
 });
