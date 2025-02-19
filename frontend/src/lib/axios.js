df7207dbe812d8815d7e0996982fc7fd21e68489
Prakhar-Ordex <prakhar.tripathi.ordex@gmail.com>
2025-02-19 14:57:46 +0530

    update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:  "http://localhost:3000/api",
+  baseURL:  "https://chat-app-t0e4.onrender.com/api",
   withCredentials: true,
 });
