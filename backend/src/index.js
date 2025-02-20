7e866477323bc418496039224b58d3c25ceeae56
Prakhar-Ordex <prakhar.tripathi.ordex@gmail.com>
2025-02-20 10:22:00 +0530

    update dev

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://chattya.netlify.app",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:  "https://chat-app-8wm6.onrender.com/api",
+  baseURL:  "http://localhost:3000/api",
   withCredentials: true,
 });
